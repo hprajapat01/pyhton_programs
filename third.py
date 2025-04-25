261d6e6b961ff97b3ea72af0b2ad86b892878fbf
Klizard Technology <klizardtechnology@gmail.com>
2025-04-25 16:48:31 +0530

    add one more program

---
+++ third.py
@@ -0,0 +1,9 @@
+a=input("Enter a number: ")
+b=input("Enter another number: ")
+c = input("Enter anotehr number: ")
+if(a>b and a>c):
+    print(a,"is the largest number")
+elif(b>a and b>c):
+    print(b,"is the larget number")
+else:
+    print(c,"is the larget number")
