9019e2685de81766b36d1102fcb201f0dec797cf
Klizard Technology <klizardtechnology@gmail.com>
2025-04-25 17:03:34 +0530

    add one more basic program

---
+++ fourth.py
@@ -0,0 +1,7 @@
+a=int(input("Enter first side of the triangle:"))
+b=int(input("Enter second side of the triangle:"))
+c=int(input("Enter third side of the triangle:"))
+if((a+b+c)==180):
+    print("It is a valid triangle.")
+else:
+    print("It is not a valid triangle.")
